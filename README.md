e620abaa6619f1be1ef856865e7418068a1a3671
Aureliano Yepez <yepez4845@gmail.com>
2018-02-24 17:16:40 -0800

    ReadMe UPDATED

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # manageMePy
+## A command line budgeting tool
+userScript.py and Items.py are the two main files I am currently working with. Finding proper
+data structures to store Object Oriented Data and cleaning up the code are two of my current priorities.
+
+I mainly worked on this project over summer on my free time in hopes that it would help me track my
+spending in college.
+
+So far I was able to get user input, then convert the user input into object oriented structures in Python,
+then get the data from the object oriented functions and input the data into an excel sheet using Pandas,
+and DataFrame.
+
+As of right now, the project works. However, like I have mentioned before I want to
+* Increase modularity
+* Use more effective data structures
+* Implement a pdf command that includes more info about an individuals spending
+* Use less redundant code
--- Src/backend/account.py
+++ Src/backend/account.py
@@ -1,43 +1,41 @@
-## File Description ## 
-## Author: Aureliano Yepez 
+## File Description ##
+## Author: Aureliano Yepez
 ## Date: July 22, 2017
 """
-
+Parse purchases from banks ? First complete what is needed ! 
 
 """
 
 class Account:
 
-	numberWithdraws = 0 
+	numberWithdraws = 0
 	numberDeposits =  0
-	interest = 0.2 # will not be used for now ... 
-	hasBank = True 
-	isValidBank =  False  
+	interest = 0.2 # will not be used for now ...
+	hasBank = True
+	isValidBank =  False
 	country =" United States"
-	#not all the banks -- few major ones 
+	#not all the banks -- few major ones
 	banks = ["chase","bank of america", "wells fargo","citi bank","goldman sachs","morgan stanley",
 			"bancorp", "capitol one"]
 
 	def __init__(self,name,bank):
-		self.name = name 
-		# if self.bank in banks: 
-		self.bank = bank 
+		self.name = name
+		# if self.bank in banks:
+		self.bank = bank
 		self.balance = 0
 
 
 	def withdraw(self, amount):
-		
-		self.balance = self.balance - amount 
+
+		self.balance = self.balance - amount
 		if (self.balance > 0):
 			print("Your current amount is: " + str(self.balance))
-		else: 
+		else:
 			raise AssertionError("Not enough money to withdraw")
 		Account.numberWithdraws +=  1
 
 
 	def deposit(self, amount):
-		self.balance = self.balance + amount 
+		self.balance = self.balance + amount
 		print("Your current amount is: " + str(self.balance))
-		Account.numberDeposits += 1 
-
-	
+		Account.numberDeposits += 1
